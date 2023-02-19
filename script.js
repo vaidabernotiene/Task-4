00b9d2d12b46950ae7fd924f847bf5b2f340d802
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-19 18:55:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/* ------------------------------ TASK 4 -----------------------------------
+Parašykite JS kodą, kuris vartotojui atėjus į tinklalapį kreipsis į cars.json failą
+ir iš jo atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
+Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
+nurodomas gamintojas ir jo pagaminti modeliai.
+
+
+Pastaba: Informacija apie automobilį (brand) (jo kortelė) turi turėti 
+bent minimalų stilių;
+-------------------------------------------------------------------------- */
+const divOutputEl = document.querySelector("#output");
+
+const ENDPOINT = "./cars.json";
+
+async function getCarsData() {
+  
+}
+
+getCarsData();
